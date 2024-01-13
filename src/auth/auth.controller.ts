2c8dfeb46ea44c22a182d6041913f173a4ae6011
Habit <char.habit@gmail.com>
2024-01-14 01:30:09 +0400

    feat: Create users entity; connect to DB; connected and configured migrations; add cookie parser; swagger dock. init; register endpoint init;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post, Res} from '@nestjs/common';
+import {CreateUserDto} from './dtos/create-user.dto';
+import {Response} from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('/registration')
+  registration(
+    @Body() userData: CreateUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return 'register';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Test project')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
