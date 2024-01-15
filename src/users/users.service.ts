8ac069579ff38c13cb1ea798c95557a9e1e59a99
Habit <char.habit@gmail.com>
2024-01-15 12:26:58 +0400

    feat: add create user func; refactor: linter fix;

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersEntity } from '../database/entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async createUser(userData: CreateUserDto) {
+    const isUser = await this.usersRepository.findOne({
+      where: { email: userData.email },
+    });
+    if (isUser)
+      throw new HttpException(
+        'Такой пользователь уже существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    return this.usersRepository.save({
+      ...userData,
+    });
+  }
+}
