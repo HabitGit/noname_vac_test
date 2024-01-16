c926f4243e447f418ee08fdf4fa0d75973c062a6
Habit <char.habit@gmail.com>
2024-01-16 18:08:43 +0400

    feat: add docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,14 @@ import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Авторизация')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Регистрация юзера' })
   @Post('/registration')
   async registration(
     @Body() userData: CreateUserDto,
@@ -14,6 +17,8 @@ export class AuthController {
   ) {
     return this.authService.registration(userData, response);
   }
+
+  @ApiOperation({ summary: 'Логин юзера' })
 
   @Post('/login')
   async login(
@@ -23,6 +28,7 @@ export class AuthController {
     return this.authService.login(userData, response);
   }
 
+  @ApiOperation({ summary: 'Логаут юзера' })
   @Post('/logout')
   logout(
     @Req() request: Request,
@@ -32,6 +38,7 @@ export class AuthController {
     return this.authService.logout(refreshToken, response);
   }
 
+  @ApiOperation({ summary: 'Обновление токена юзера' })
   @Post('/refresh')
   refreshTokens(
     @Req() request: Request,
